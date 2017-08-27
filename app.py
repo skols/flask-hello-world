640df250a4b8a1c98433fb94f696f77dd675de8d
Michael Skolnik <mikeskolnik75@gmail.com>
2017-08-26 20:29:18 -0400

    Second commit - Added dynamic routes and responses

--- app.py
+++ app.py
@@ -3,18 +3,53 @@
 # import the Flask class from the flask package
 from flask import Flask
 
-# Create the application object
+# create the application object
 app = Flask(__name__)
 
 
-# Use the decorator pattern to link the view function to a URL
+# use the decorator pattern to link the view function to a URL
 @app.route("/")
 @app.route("/hello")
-# Definte the view using a function, which returns a string
+# definte the view using a function, which returns a string
 def hello_world():
     return "Hello, World!"
 
 
-# Start the development server using the run command
+# dynamic route
+@app.route("/test/<search_query>")
+def search(search_query):
+    return search_query
+
+
+# dynamic route - int
+@app.route("/integer/<int:value>")
+def int_type(value):
+    print(value + 1)
+    return "correct"
+
+
+# dynamic route - int
+@app.route("/float/<float:value>")
+def float_type(value):
+    print(value + 1)
+    return "correct"
+
+
+# dynamic route that accepts slashes
+@app.route("/path/<path:value>")
+def path_type(value):
+    print(value)
+    return "correct"
+
+
+@app.route("/name/<name>")
+def index(name):
+    if name.lower() == "michael":
+        return "Hello, {}".format(name), 200
+    else:
+        return "Not Found", 404
+
+
+# start the development server using the run command
 if __name__ == "__main__":
     app.run()
